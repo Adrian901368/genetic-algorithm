bd752f1f8bbc5cf1eee65a2d5fe0c26522e33cab
Adrian901368 <145666204+Adrian901368@users.noreply.github.com>
2024-09-21 23:12:39 +0200

    moving basics

--- findtreasure.py
+++ findtreasure.py
@@ -16,37 +16,45 @@ def count_ones(binary_number: str) -> int:
 def binary_to_decimal(binary_number: str) -> int:
     return int(binary_number, 2)
 
+
+def move_finder(max_steps = 100):
+    steps = 0
+    ix_adress = 0
+
+    while steps < max_steps:
+        position = ix_adress % 64
+        value = instruction_table[position]
+        instruction = value[:2]
+        next_val_position = binary_to_decimal(value[2:])
+        next_value = instruction_table[next_val_position]
+
+        if instruction == '00':  # Increment
+            instruction_table[next_val_position] = increment(next_value)
+            ix_adress += 1
+        elif instruction == '01':  # Decrement
+            instruction_table[next_val_position] = decrement(next_value)
+            ix_adress += 1
+        elif instruction == '10':  # Jump
+            ix_adress = binary_to_decimal(value[2:])
+        else:  # Print
+            if count_ones(next_value) <= 2:
+                print("H", end=" ")
+            elif count_ones(next_value) <= 4:
+                print("D", end=" ")
+            elif count_ones(next_value) <= 6:
+                print("P", end=" ")
+            else:
+                print("L", end=" ")
+
+            ix_adress += 1
+        steps += 1
+
+
+
 instruction_table = []
 for i in range(64):
     binary_number = format(randint(0, 255), '08b')
     instruction_table.append(binary_number)
 
-steps = 0
-ix_adress = 0
 
-while steps < 100:
-    position = ix_adress % 64
-    value = instruction_table[position]
-    instruction = value[:2]
-    next_val_position = binary_to_decimal(value[2:])
-    next_value = instruction_table[next_val_position]
-
-    if instruction == '00':  # Increment
-        instruction_table[next_val_position] = increment(next_value)
-        ix_adress += 1
-    elif instruction == '01':  # Decrement
-        instruction_table[next_val_position] = decrement(next_value)
-        ix_adress += 1
-    elif instruction == '10':  # Jump
-        ix_adress = binary_to_decimal(value[2:])
-    else:  # Print
-        if count_ones(next_value) <= 2:
-            print("H", end=" ")
-        elif count_ones(next_value) <= 4:
-            print("D", end=" ")
-        elif count_ones(next_value) <= 6:
-            print("P", end=" ")
-        else:
-            print("L", end=" ")
-        ix_adress += 1
-    steps += 1
+move_finder(500)
